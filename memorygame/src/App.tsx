0b030f8d9019149ab8472b60e48d17ec64963a50
Dawa <sherpadawa01@outlook.com>
2022-11-28 21:46:41 +0545

    desinging the grid

--- memorygame/src/App.tsx
+++ memorygame/src/App.tsx
@@ -1,13 +1,27 @@
 import { useState } from 'react';
-
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">
-    Hello World
-  </div>;
+  const [grid, setGrid] = useState([
+    [0, 3, 5, 1],
+    [1, 2, 2, 4],
+    [4, 3, 5, 0],
+  ]);
+  return (
+    <div className="App">
+      <div className="grid">
+        {grid.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((column, columnIndex) => (
+              <div key={columnIndex} className="card">
+                {column}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
